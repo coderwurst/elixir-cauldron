08c549082fe404d9807550086cfb38a7a057b18d
Andrew McMechan <coderwurst@users.noreply.github.com>
2017-06-18 12:14:18 +0200

    add recursion to math module

--- math.ex
+++ math.ex
@@ -14,4 +14,20 @@ defmodule Math do
   def zero?(x) when is_integer(x) do
     false
   end
+
+  def sum_list([head | tail], accumulator) do
+    sum_list(tail, head + accumulator)
+  end
+
+  def sum_list([], accumulator) do
+    accumulator
+  end
+ 
+  def double_each([head | tail]) do
+    [head * 2 | double_each(tail)]
+  end
+
+  def double_each([]) do
+    []
+  end
 end
