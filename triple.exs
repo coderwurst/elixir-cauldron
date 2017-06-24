4ef9b2d6608eb4da0929335bf4a772645cd1190e
Andrew McMechan <coderwurst@users.noreply.github.com>
2017-06-24 19:20:58 +0200

    added module doc to Triples

--- triple.exs
+++ triple.exs
@@ -1,6 +1,9 @@
 defmodule Triple do
   @moduledoc """
   Used to generate Pythagorean triples (a*a + b*b = c*c)
+  n <- [1, 2, 3, 4] is an example of a generator
+  in example below, filters are used to determine if eg,
+  a*a + b*b == c*c
   """
   def pythagorean(n) when n > 0 do
     for a <- 1..n,
