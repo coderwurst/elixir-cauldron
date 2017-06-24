2915746431ecc17eb76919b7d29a0a7a082f4c4a
Andrew McMechan <coderwurst@users.noreply.github.com>
2017-06-24 18:55:34 +0200

    replace size() implementation with own

---
+++ size.ex
@@ -0,0 +1,20 @@
+defprotocol Size do
+  @doc "Calculates the size (and not the length!) of a data structure"
+  def size(data)
+end
+
+defimpl Size, for: BitString do
+  @doc "Calculates the size (and not the length!) of a data String"
+  def size(string), do: byte_size(string)
+end
+
+defimpl Size, for: Map do
+  @doc "Calculates the size (and not the length!) of a map"
+  def size(map), do: map_size(map)
+end
+
+defimpl Size, for: Tuple do
+  @doc "Calculates the size (and not the length!) of a tuple"
+  def size(tuple), do: tuple_size(tuple)
+end
+
