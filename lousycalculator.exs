b6c4977a159262824db28583da4c7d05dd92de12
Andrew McMechan <coderwurst@users.noreply.github.com>
2017-06-25 15:47:46 +0200

    update LousyCal to use @Type directive

--- lousycalculator.exs
+++ lousycalculator.exs
@@ -1,8 +1,12 @@
-
 defmodule LousyCalculator do
-  @spec add(number, number) :: {number, String.t}
-  def add(x, y), do: {x + y, "You need a calculator to do that?!"}
+  @typedoc """
+  Just a number followed by a string.
+  """
+  @type number_with_remark :: {number, String.t}
 
-  @spec multiply(number, number) :: {number, String.t}
-  def multiply(x, y), do: {x * y, "Jeez, come on!"}
+  @spec add(number, number) :: number_with_remark
+  def add(x, y), do: {x + y, "You need a calculator to do that?"}
+
+  @spec multiply(number, number) :: number_with_remark
+  def multiply(x, y), do: {x * y, "It is like addition on steroids."}
 end
